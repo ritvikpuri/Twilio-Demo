21fc4d43f5c07fe7d2101c83e1a02332a4bee0dd
Ritvik.Puri <ritvikpuri00@gmail.com>
2019-06-27 16:53:11 +0530

    v1

--- twilio-demo/src/main/java/com/example/demo/TwilioDemoApplication.java
+++ twilio-demo/src/main/java/com/example/demo/TwilioDemoApplication.java
@@ -11,9 +11,9 @@ import com.twilio.type.PhoneNumber;
 @SpringBootApplication
 public class TwilioDemoApplication implements CommandLineRunner{
 
-	public static final String ACCOUNT_SID = "AC3973b3cf024f0030cee6f55a5d538d4a";
-	public static final String AUTH_TOKEN = "725ac420c923be45c891a983be7d598a";
-	public static final String TWILIO_NUMBER = "+12566002122";
+	public static final String ACCOUNT_SID = "XXXXX";
+	public static final String AUTH_TOKEN = "XXXX";
+	public static final String TWILIO_NUMBER = "+XXXX";
 	
 	
 	public static void main(String[] args) {
@@ -23,6 +23,6 @@ public class TwilioDemoApplication implements CommandLineRunner{
 	@Override
 	public void run(String...args) throws Exception{
 		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-		Message message = Message.creator(new PhoneNumber("+919716250272"), new PhoneNumber(TWILIO_NUMBER),"THIS WORKS?").create();
+		Message message = Message.creator(new PhoneNumber("+XXXX"), new PhoneNumber(TWILIO_NUMBER),"THIS WORKS?").create();
 	}
 }
